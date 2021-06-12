b9f970707fc55bdc6473879640802c23479a70ef
unknown <anthonybeli1999@gmail.com>
2021-06-11 20:34:41 -0500

    Version 1.12.6 Detalles admin, articulos, enfoque psicologico

--- app/src/main/java/com/example/helpsych/Fragments/ArticleAdminFragment.java
+++ app/src/main/java/com/example/helpsych/Fragments/ArticleAdminFragment.java
@@ -4,7 +4,10 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -13,21 +16,31 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.helpsych.Activity.ArticleDetails;
 import com.example.helpsych.Activity.LoginActivity;
 import com.example.helpsych.Activity.PopupAddArticle;
 import com.example.helpsych.Activity.PreviewArticleActivity;
 import com.example.helpsych.Activity.RegisterActivity;
 import com.example.helpsych.Model.Article;
+import com.example.helpsych.Model.User;
 import com.example.helpsych.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -40,12 +53,16 @@ public class ArticleAdminFragment extends Fragment {
     private EditText Article_title, Article_subtitle ,Article_body, Article_date, Article_author, Article_label;
 
     ImageView ArticleImage;
-    private Button AddNewArticle, AddNewImageArticle;
+    private Button AddNewImageArticle;
+    private FloatingActionButton AddNewArticle;
     private DatabaseReference RootRef;
 
     private ProgressDialog loadingBar;
 
     private DatabaseReference ArticleRef;
+
+    private RecyclerView ArticlesRecyclerList;
+    private DatabaseReference ArticlesRef;
 
     private static final int GalleryPick = 1;
 
@@ -87,14 +104,60 @@ public class ArticleAdminFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_article_admin, container, false);
 
-        View v = inflater.inflate(R.layout.fragment_article_admin, container, false);
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         RootRef = FirebaseDatabase.getInstance().getReference();
-
         ArticleRef = FirebaseDatabase.getInstance().getReference().child("Article");
+        AddNewArticle = (FloatingActionButton) rootView.findViewById(R.id.article_add_admin_new);
 
-        AddNewArticle = (Button) v.findViewById(R.id.article_add_admin_new);
+        ArticlesRef = FirebaseDatabase.getInstance().getReference().child("Article");
+        ArticlesRecyclerList = rootView.findViewById(R.id.article_recycler_list);
+
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseRecyclerOptions<Article> options =
+                new FirebaseRecyclerOptions.Builder<Article>()
+                        .setQuery(ArticlesRef, Article.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Article, ArticleAdminFragment.ArticlesViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Article, ArticleAdminFragment.ArticlesViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ArticleAdminFragment.ArticlesViewHolder holder, final int position, @NonNull Article model)
+                    {
+                        Picasso.get().load(model.getImage()).placeholder(R.drawable.article).into(holder.article_image);
+                        holder.article_approach.setText(model.getApproach());
+                        holder.article_title.setText(model.getTitle());
+                        holder.article_date.setText(model.getDate());
+
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                String visit_article_id = getRef(position).getKey();
+                                Intent profileIntent = new Intent(getContext(), ArticleDetails.class);
+                                profileIntent.putExtra("id_article", visit_article_id);
+                                startActivity(profileIntent);
+                            }
+                        });
+                    }
+
+                    @NonNull
+                    @Override
+                    public ArticleAdminFragment.ArticlesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i)
+                    {
+                        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.article_display_layout, viewGroup, false);
+                        ArticleAdminFragment.ArticlesViewHolder viewHolder = new ArticleAdminFragment.ArticlesViewHolder(view);
+                        return viewHolder;
+                    }
+                };
+
+        ArticlesRecyclerList.setLayoutManager(new GridLayoutManager(getContext(),1));
+        ArticlesRecyclerList.setAdapter(adapter);
+
+        adapter.startListening();
+
 
         AddNewArticle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -107,7 +170,23 @@ public class ArticleAdminFragment extends Fragment {
         Date c = Calendar.getInstance().getTime();
         SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         formattedDate = df.format(c);
-        return v;
+        return rootView;
     }
 
+    public static class ArticlesViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView article_approach;
+        TextView article_title;
+        TextView article_date;
+        ImageView article_image;
+
+        public ArticlesViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+            article_approach = itemView.findViewById(R.id.txt_article_psyappoach);
+            article_title = itemView.findViewById(R.id.txt_article_title);
+            article_date = itemView.findViewById(R.id.txt_article_date);
+            article_image = itemView.findViewById(R.id.img_article_image);
+        }
+    }
 }
